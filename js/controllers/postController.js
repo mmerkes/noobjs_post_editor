474428e16a4754cd6a764b6a2f772c19392aa2bf
Empty Arsenal <matt.merkes@gmail.com>
2013-11-14 21:06:52 -0800

    Editing posts now works

--- js/controllers/postController.js
+++ js/controllers/postController.js
@@ -1,1 +1,7 @@
-App.PostController = Ember.ObjectController.extend();
+App.PostController = Ember.ObjectController.extend({
+	actions: {
+		edit: function() {
+			this.transitionToRoute('post.edit');
+		}
+	}
+});
