3f5614acdac442672cf32a406d53f039e5d45ec3
Empty Arsenal <matt.merkes@gmail.com>
2013-11-15 12:53:52 -0800

    Added edit and delete routes, and restructured HTML to have posts bar on the side

--- js/controllers/postController.js
+++ js/controllers/postController.js
@@ -2,6 +2,23 @@ App.PostController = Ember.ObjectController.extend({
 	actions: {
 		edit: function() {
 			this.transitionToRoute('post.edit');
-		}
+		},
+		delete: function(){
+	      	// our delete method now only toggles deleteMode's value
+	      	this.toggleProperty('deleteMode');
+	    },
+	    cancelDelete: function(){
+	      	// set deleteMode back to false
+	      	this.set('deleteMode', false);
+	    },
+	    confirmDelete: function(){
+	      	// this tells Ember-Data to delete the current user
+	      	this.get('model').deleteRecord();
+	      	this.get('model').save();
+	      	// and then go to the users route
+	      	this.transitionToRoute('posts');
+	      	// set deleteMode back to false
+	      	this.set('deleteMode', false);
+	    }
 	}
 });
